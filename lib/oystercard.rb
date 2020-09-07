58ec05d9fd9dfa7b7d5fa8ca5a589086ca6a3311
Charlotte Brandhorst-Satzkorn <catzkorn@gmail.com>
2020-09-07 16:38:04 +0100

    added deduct test and method

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,15 +1,18 @@
 class Oystercard
   attr_reader :balance
 
-LIMIT = 90
+  LIMIT = 90
 
-  def initialize(balance=0)
+  def initialize(balance = 0)
     @balance = balance
   end
 
   def top_up(amount)
-    fail "Card reached the £#{LIMIT} limit" if @balance + amount > LIMIT
+    fail "Card reached the GBP#{LIMIT} limit" if @balance + amount > LIMIT
     @balance + amount
   end
 
+  def deduct(amount)
+    @balance - amount
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,19 +1,25 @@
-require 'oystercard'
+require "oystercard"
 
 describe Oystercard do
-  it 'has a default balance of 0' do
+  it "has a default balance of 0" do
     expect(subject.balance).to eq 0
   end
 
-  describe '#top_up' do
-    it 'adds an amount to a card balance' do
+  describe "#top_up" do
+    it "adds an amount to a card balance" do
       amount = 30
       expect(subject.top_up(amount)).to eq 30
     end
 
-    it "raises an error if card topped up more than £90" do
-      expect { subject.top_up(Oystercard::LIMIT + 1) }.to raise_error "Card reached the £#{Oystercard::LIMIT} limit"
+    it "raises an error if card topped up more than GBP90" do
+      expect { subject.top_up(Oystercard::LIMIT + 1) }.to raise_error "Card reached the GBP#{Oystercard::LIMIT} limit"
     end
   end
 
+  describe "#deduct" do
+    it "deducts a specified amount" do
+      card = Oystercard.new(30)
+      expect(card.deduct(5)).to eq 25
+    end
+  end
 end
